b442060e49b8b2075c007e5337ad18a865067e3b
Dmitry Ryadnenko <dmitry.ryadnenko@screeninteraction.com>
2016-06-07 16:13:04 +0200

    --no-ff merge

--- test/test_all.rb
+++ test/test_all.rb
@@ -7,7 +7,7 @@ class TC_All < Test::Unit::TestCase
 
     `git init ../test-output/git-repository`
     `git init --bare ../test-output/git-repository.git`
-    Dir.chdir('../test-output/git-repository'){
+    Dir.chdir('../test-output/git-repository') {
       `git checkout -b develop`
       `touch file1`
       `git add file1`
@@ -19,11 +19,11 @@ class TC_All < Test::Unit::TestCase
   end
 
   def teardown
-    `rm -rf ../test-output`
+    # `rm -rf ../test-output`
   end
 
   def test__on_develop_branch__exit
-    Dir.chdir('../test-output/git-repository'){
+    Dir.chdir('../test-output/git-repository') {
       puts `../../merge-fix`
 
       exit_status = $? >> 8
@@ -34,7 +34,7 @@ class TC_All < Test::Unit::TestCase
   end
 
   def test__merges_alright
-    Dir.chdir('../test-output/git-repository'){
+    Dir.chdir('../test-output/git-repository') {
       `git checkout -b fix1`
       `echo 'som fix' >> file1`
       `git add .`
@@ -49,7 +49,7 @@ class TC_All < Test::Unit::TestCase
   end
 
   def test__master_has_changes__merges_alright
-    Dir.chdir('../test-output/git-repository'){
+    Dir.chdir('../test-output/git-repository') {
       `git checkout -b fix1`
       `echo 'som fix' >> file2`
       `git add .`
@@ -69,6 +69,21 @@ class TC_All < Test::Unit::TestCase
 
       assert_equal(0, $?)
       assert_equal("fixed a little\nfixed a little 2\nfirst commit", `git log --pretty=format:"%s"`)
+      assert_equal('refs/heads/develop', `git symbolic-ref HEAD`.strip)
+    }
+  end
+
+  def test__no_ff__merges_alright
+    Dir.chdir('../test-output/git-repository') {
+      `git checkout -b fix1`
+      `echo 'som fix' >> file1`
+      `git add .`
+      `git commit -m "fixed a little"`
+
+      puts `../../merge-fix --no-ff`
+
+      assert_equal(0, $?)
+      assert_equal("Merge branch 'fix1' into develop\nfixed a little\nfirst commit", `git log --pretty=format:"%s" --topo-order`)
       assert_equal('refs/heads/develop', `git symbolic-ref HEAD`.strip)
     }
   end
